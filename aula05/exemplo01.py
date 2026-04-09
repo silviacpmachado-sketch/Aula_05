366a8c8e507116e69e41b325a7878d6e81d1aa6d
silviacpmachado-sketch <silviacpmachado@gmail.com>
2026-04-09 20:40:09 -0300

    Inicializa

--- aula05/exemplo01.py
+++ aula05/exemplo01.py
@@ -23,34 +23,3 @@
 #     case _: 
 #         print('Acesso Negado')
 
-# Match Case
-print("""
-[1] - Sul
-[2] - Norte
-[3] - Leste
-[4] - Oeste
-[5 a 6] - Nordeste
-[7 a 9] - Sudeste
-[10] - Centro-Oeste
-[11] - Noroeste
- """)
-
-codigo = int(input('Informe o código de acesso: '))
-
-match codigo: 
-    case 1:
-        print('Sul')
-    case 2:
-        print('Norte')
-    case 3:
-        print('Leste')
-    case 4:
-        print('Oeste')
-    case 5 | 6:
-        print('Nordeste')
-    case 7 | 8 | 9:
-        print('Centro-Oeste')
-    case 10:
-        print('Noroeste')
-    case _: 
-        print('Importado')
